8a0116efad3943740ed992f84e616fcb343a04f4
saurav0705 <sauravaggarwal98@gmail.com>
2020-05-05 12:35:39 +0530

    done task without redux

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [selected,setSelected] = useState([]);
+  const cards = [1,2,3,4,5,6,7,8,9,10];
+  useEffect(()=>{
+    let obj = [];
+    if(localStorage.getItem('selected')){
+      obj= JSON.parse(localStorage.getItem('selected'));
+
+    }else{
+   for(let i=0;i<21;i++){ 
+      obj.push(false);
+    }}
+
+    setSelected([...obj]);
+  },[])
+
+  const select = (num) => {
+      let obj = selected;
+      obj[num] = !obj[num];
+      setSelected([...obj])
+      localStorage.setItem('selected',JSON.stringify(selected));
+
+  }
+
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="flex">
+        <div className="left">
+
+        {cards.map(card => {
+          return(
+          <div className={selected[card*2] ?"tile selected":"tile left-tile"} onClick={()=>select(card*2)} key={card*2}>{card*2}</div>
+          )
+        })}
+        </div>
+        <div className="right ">
+        {cards.map(card => {
+          return(
+          <div className={selected[(card*2)+1] ? "tile selected":"tile right-tile"} onClick={()=>select((card*2) + 1)} key={(card*2) + 1}>{(card*2) + 1}</div>
+          )
+        })}
+        </div>
+      </div>
+
+      
     </div>
   );
 }
